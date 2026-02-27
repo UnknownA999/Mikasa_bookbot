0aa3a820754001fcc7f0f3755e92625aecb6079b
UnknownA999 <102337915+UnknownA999@users.noreply.github.com>
2026-02-27 09:59:07 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -80,11 +80,8 @@ async def dreamxbotz_start():
     now = datetime.now(tz)
     time = now.strftime("%H:%M:%S %p")
     await dreamxbotz.send_message(chat_id=LOG_CHANNEL, text=script.RESTART_TXT.format(temp.B_LINK, today, time))
-    app = web.AppRunner(await web_server())
-    await app.setup()
-    bind_address = "0.0.0.0"
-    await web.TCPSite(app, bind_address, PORT).start()
-#    dreamxbotz.loop.create_task(keep_alive())
+    print("📚 Starting Background Book Scraper...")
+    asyncio.create_task(background_book_scraper(dreamxbotz, db))
     await idle()
     
 if __name__ == '__main__':
