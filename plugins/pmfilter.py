64720669f3749ee2a8500f43b0287c8045103974
UnknownA999 <102337915+UnknownA999@users.noreply.github.com>
2026-06-03 20:10:06 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -976,13 +976,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 except Exception:
                     verify = verify_url
                     
-                buttons = [[
-                    InlineKeyboardButton(text="♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪꜰʏ ♻️", url=verify)
-                ],[
-                    InlineKeyboardButton(text="⁉️ ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ ⁉️", url=settings.get('tutorial', TUTORIAL) if settings else TUTORIAL)
-                ]]
-                    InlineKeyboardButton(text="⭐ GO AD-FREE / BUY PREMIUM ⭐", url=f"https://t.me/{temp.U_NAME}?start=premium")
-                ]]
+                buttons = [
+                    [InlineKeyboardButton(text="♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪꜰʏ ♻️", url=verify)],
+                    [InlineKeyboardButton(text="⁉️ ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ ⁉️", url=settings.get('tutorial', TUTORIAL) if settings else TUTORIAL)],
+                    [InlineKeyboardButton(text="⭐ GO AD-FREE / BUY PREMIUM ⭐", url=f"https://t.me/{temp.U_NAME}?start=premium")]
+                ]
+
                 
                 await query.message.reply_text(
                     text=f"📌 **{query.from_user.mention}, ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴠᴇʀɪꜰɪᴇᴅ!**\n\nᴘʟᴇᴀꜱᴇ ᴄʟɪᴄᴋ ᴏɴ 'ᴠᴇʀɪꜰʏ' ᴛᴏ ɢᴇᴛ ᴜɴʟɪᴍɪᴛᴇᴅ ᴀᴄᴄᴇꜱꜱ ꜰᴏʀ ᴛʜᴇ ɴᴇxᴛ **16 ʜᴏᴜʀꜱ**.",
